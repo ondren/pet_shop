a6652f438d7c068a210f699a6bb2a0d7dbb55724
ondren <andreykl123321@gmail.com>
2024-06-27 09:01:42 +0300

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -12,3 +12,15 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
+
+echo "# pet_shop" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ondren/pet_shop.git
+// git push -u origin main
+
+git remote add origin https://github.com/ondren/pet_shop.git
+    git branch -M main
+git push -u origin main
